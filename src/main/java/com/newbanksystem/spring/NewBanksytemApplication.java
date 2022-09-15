9a666616e0d0a671fbdb7f5fbf446d6a797b6548
abnerAmos <abner.fsouza1@gmail.com>
2022-09-15 08:38:06 -0300

    Inicio Novo Projeto

---
+++ src/main/java/com/newbanksystem/spring/NewBanksytemApplication.java
@@ -0,0 +1,13 @@
+package com.newbanksystem.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBanksytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewBanksytemApplication.class, args);
+	}
+
+}
